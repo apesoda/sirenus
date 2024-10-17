0100cfe87853da2d6b1f68b47e318c02f1984350
apesoda <43443669+apesoda@users.noreply.github.com>
2024-10-17 21:30:29 +0200

    Added basic mp3 validation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import pygame
+from mutagen.mp3 import MP3
+from mutagen import MutagenError
 
 app = Flask(__name__)
 
@@ -10,21 +12,31 @@ pygame.mixer.init()
 # Define where in the current working directory the sound folder is
 SOUND_FOLDER = os.path.join(os.getcwd(), 'static/sounds')
 
+def is_mp3(file_path):
+    try:
+        mp3_file = MP3(file_path)
+        return True
+    except MutagenError:
+        return False
+
 # Look in 'SOUND_FOLDER' for mp3's put them in an array and populate the
 # 'index.html' template with the sounds as buttons.
 @app.route('/')
 def index():
     sound_files = [f for f in os.listdir(SOUND_FOLDER) if f.endswith('.mp3')]
-    return render_template('index.html', sounds=sound_files)
+    valid_files = [f for f in sound_files if is_mp3(os.path.join(SOUND_FOLDER, f))]
+    return render_template('index.html', sounds=valid_files)
 
 # Request handling; check if requested file exists, load and play
 # Return error if request file is not found in the requested location
 @app.route('/play', methods=['POST'])
 def play_sound():
     sound_file = request.json.get('sound_file')
-    if sound_file and os.path.exists(os.path.join(SOUND_FOLDER, sound_file)):
+    file_path = os.path.join(SOUND_FOLDER, sound_file)
+
+    if sound_file and os.path.exists(file_path) and is_mp3(file_path):
         # Load and play the sound
-        pygame.mixer.music.load(os.path.join(SOUND_FOLDER, sound_file))
+        pygame.mixer.music.load(file_path)
         pygame.mixer.music.play()
         return jsonify({'status': 'playing', 'file': sound_file})
     return jsonify({'status': 'error', 'message': 'File not found'}), 404
