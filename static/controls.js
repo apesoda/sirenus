fb7cbf4d4d6da19d60beff6a207c1700ace67879
apesoda <43443669+apesoda@users.noreply.github.com>
2025-04-28 20:04:25 +0200

    Added search function, tweaked styling

--- static/controls.js
+++ static/controls.js
@@ -28,3 +28,21 @@ function stopSound() {
         console.error('Error:', error);
     });
 }
+
+//Filter search results
+function searchSound() {
+    var input, filter, sounds, results, a, i, textValue;
+    input = document.getElementById('search-box');
+    filter = input.value.toUpperCase();
+    sounds = document.getElementById('button-container');
+    results = sounds.getElementsByTagName('button');
+
+    for (i = 0; i < results.length; i++) {
+    textValue = results[i].textContent || results[i].innnerText;
+    if (textValue.toUpperCase().indexOf(filter) > -1) {
+        results[i].style.display = "";
+	} else {
+	  results[i].style.display = "none";
+	}
+    }
+}
