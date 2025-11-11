26012d1bc66a6ea43ace630b368aabc1ac8c99ba
apesoda <43443669+apesoda@users.noreply.github.com>
2025-11-11 20:24:44 +0100

    Added comments to helper script

--- helpers/svg.py
+++ helpers/svg.py
@@ -1,21 +1,32 @@
+"""
+This helper script allows us to insert inline SVG's as variables in our
+templates. This makes our templates more readable and makes SVG's easier
+to work with.
+"""
 from flask import current_app
 from markupsafe import Markup
 from pathlib import Path
 
+# Init svg cache for prod
 _svg_cache = {}
 
 def svg(name):
+    
+    # Check if app is running in debug mode
     debug = current_app.debug
 
+    # Return cached svg if present cahce in prod
     if not debug and name in _svg_cache:
         return _svg_cache[name]
 
+    # Check if requested svg is present
     svg_path = Path(current_app.root_path) / "templates" / "svg" / f"{name}.svg"
 
-
+    # Error if not present in path
     if not svg_path.exists():
         raise FileNotFoundError(f"SVG '{name}' not found at {svg_path}")
 
+    # Open and format to print
     with open(svg_path, encoding="utf-8") as file:
         content = file.read().strip()
 
